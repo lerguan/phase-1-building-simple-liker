1467bfa6b1b6aaed99ed486f171ab4492aa85f12
Leroy G <liguan698@gmail.com>
2023-04-19 21:44:08 -0700

    complete lab

--- main.js
+++ main.js
@@ -3,10 +3,29 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.getElementById('modal').setAttribute('class', 'hidden')
+document.addEventListener('click', (e) => {
+  e.preventDefault()
+  mimicServerCall()
+  .then(() =>{
+    if (e.target.innerText === EMPTY_HEART) {
+      e.target.innerText = FULL_HEART
+      e.target.setAttribute('class', 'activated-heart')
+    }
+    else {
+      e.target.innerText = EMPTY_HEART
+      e.target.removeAttribute('class', 'activate-heart')
+    }
+  })
+  .catch((error) => {
+    document.getElementById('modal').removeAttribute('class', 'hidden')
+    setTimeout(() => {
+      document.getElementById('modal').setAttribute('class', 'hidden')
+    })
+  })
+})
 
-
-
-
+console.log(mimicServerCall())
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
